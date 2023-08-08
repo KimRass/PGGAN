54fbd1900d035488957a4ad752f19a515de40c25
KimRass <rmx1000@yonsei.ac.kr>
2023-08-08 23:20:47 +0900

    Local to server

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ RESOL_N_IMAGES = {
 }
 
 ### Checkpoint
-CKPT_PATH = "/home/ubuntu/project/cv/pggan_from_scratch/checkpoints/256×256to512×512_56000.pth"
+CKPT_PATH = "/home/ubuntu/project/cv/pggan_from_scratch/checkpoints/256×256to512×512_60000.pth"
 STEP = None
 TRANS_PHASE = None
 RESOL_IDX = None
