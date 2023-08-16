5b215d2abbe4b73e826ffb40bc5003d5fde4f42b
KimRass <rmx1000@yonsei.ac.kr>
2023-08-16 16:40:20 +0900

    Resume from 512to1024 step 500

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ IMG_SIZE_N_IMAGES = {
 }
 
 ### Checkpoint
-CKPT_PATH = "/home/ubuntu/project/cv/pggan_from_scratch/checkpoints/512×512_266666.pth"
+CKPT_PATH = "/home/ubuntu/project/cv/pggan_from_scratch/checkpoints/512×512to×1024×1024_500.pth"
 STEP = None
 TRANS_PHASE = None
 IMG_SIZE_IDX = None
