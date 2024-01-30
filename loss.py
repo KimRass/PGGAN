e50edd2ef0f9c1d7ba1f4ae394954767b7e0602b
KimRass <rmx1000@yonsei.ac.kr>
2024-01-30 16:46:31 +0900

    README

--- loss.py
+++ loss.py
@@ -6,14 +6,14 @@ import torch
 
 # "We use the improved Wasserstein loss."
 def get_gradient_penalty(disc, img_size, alpha, real_image, fake_image):
-    eps = torch.rand(1, device=real_image.device)
-    avg_image = eps * real_image + (1 - eps) * fake_image
-    avg_image.requires_grad = True
-    avg_pred = disc(avg_image, img_size=img_size, alpha=alpha)
+    eps = torch.rand((real_image.size(0), 1, 1, 1), device=real_image.device)
+    inter_image = eps * real_image + (1 - eps) * fake_image
+    inter_image.requires_grad = True
+    inter_pred = disc(inter_image, img_size=img_size, alpha=alpha)
 
-    real_label = torch.ones_like(avg_pred, device=avg_pred.device)
+    real_label = torch.ones_like(inter_pred, device=inter_pred.device)
     grad = torch.autograd.grad(
-        outputs=avg_pred, inputs=avg_image, grad_outputs=real_label, create_graph=True, retain_graph=True
+        outputs=inter_pred, inputs=inter_image, grad_outputs=real_label, create_graph=True, retain_graph=True,
     )[0]
     grad = grad.view(grad.size(0), -1)
     gp = ((grad.norm(2, dim=1) - 1) ** 2).mean()
--- train.py
+++ train.py
@@ -20,9 +20,7 @@ from utils import (
 from model import Generator, Discriminator
 from celebahq import get_dataloader
 from loss import get_gradient_penalty
-from evaluate import get_swd
-
-# torch.autograd.set_detect_anomaly(True)
+from eval import get_swd
 
 
 # "We use a minibatch size $16$ for resolutions $4^{2}$–$128^{2}$ and then gradually decrease
@@ -53,20 +51,20 @@ def get_alpha(step, n_steps, trans_phase):
 
 
 @torch.no_grad()
-def validate(gen, val_dl, device):
+def validate(G, val_dl, device):
     print(f"Validating...")
-    gen.eval()
+    G.eval()
     sum_swd = 0
     for real_image in tqdm(val_dl):
         real_image = real_image.to(device)
         noise = torch.randn(batch_size, 512, 1, 1, device=device)
-        fake_image = gen(noise, img_size=img_size, alpha=alpha)
+        fake_image = G(noise, img_size=img_size, alpha=alpha)
 
         swd = get_swd(real_image, fake_image, device=device)
         sum_swd += swd.item()
     avg_swd = sum_swd / len(val_dl)
     print(f"Average SWD: {avg_swd: .3f}")
-    gen.train()
+    G.train()
     return avg_swd
 
 
@@ -74,10 +72,10 @@ def save_checkpoint(
     img_size_idx,
     step,
     trans_phase,
-    disc,
-    gen,
-    disc_optim,
-    gen_optim,
+    D,
+    G,
+    D_optim,
+    G_optim,
     scaler,
     avg_swd,
     save_path,
@@ -88,17 +86,17 @@ def save_checkpoint(
         "image_size_index": img_size_idx,
         "step": step,
         "transition_phase": trans_phase,
-        "D_optimizer": disc_optim.state_dict(),
-        "G_optimizer": gen_optim.state_dict(),
+        "D_optimizer": D_optim.state_dict(),
+        "G_optimizer": G_optim.state_dict(),
         "scaler": scaler.state_dict(),
         "average_swd": avg_swd,
     }
     if config.N_GPUS > 1 and config.MULTI_GPU:
-        ckpt["D"] = disc.module.state_dict()
-        ckpt["G"] = gen.module.state_dict()
+        ckpt["D"] = D.module.state_dict()
+        ckpt["G"] = G.module.state_dict()
     else:
-        ckpt["D"] = disc.state_dict()
-        ckpt["G"] = gen.state_dict()
+        ckpt["D"] = D.state_dict()
+        ckpt["G"] = G.state_dict()
 
     torch.save(ckpt, str(save_path))
 
@@ -111,15 +109,15 @@ if __name__ == "__main__":
     CKPT_DIR = ROOT_DIR/"checkpoints"
     IMG_DIR = ROOT_DIR/"generated_images"
 
-    disc = Discriminator()
-    gen = Generator()
+    D = Discriminator()
+    G = Generator()
     if config.N_GPUS > 0:
         DEVICE = torch.device("cuda")
-        disc = disc.to(DEVICE)
-        gen = gen.to(DEVICE)
+        D = D.to(DEVICE)
+        G = G.to(DEVICE)
         if config.N_GPUS > 1 and config.MULTI_GPU:
-            disc = nn.DataParallel(disc)
-            gen = nn.DataParallel(gen)
+            D = nn.DataParallel(D)
+            G = nn.DataParallel(G)
 
             print(f"Using {config.N_GPUS} GPUs.")
         else:
@@ -131,14 +129,14 @@ if __name__ == "__main__":
     # and $\epsilon = 10^{-8}$. We do not use any learning rate decay or rampdown, but for visualizing
     # generator output at any given point during the training, we use an exponential running average
     # for the weights of the generator with decay $0.999$."
-    disc_optim = Adam(
-        params=disc.parameters(),
+    D_optim = Adam(
+        params=D.parameters(),
         lr=config.LR,
         betas=(config.BETA1, config.BETA2),
         eps=config.ADAM_EPS,
     )
-    gen_optim = Adam(
-        params=gen.parameters(),
+    G_optim = Adam(
+        params=G.parameters(),
         lr=config.LR,
         betas=(config.BETA1, config.BETA2),
         eps=config.ADAM_EPS,
@@ -151,13 +149,13 @@ if __name__ == "__main__":
         ckpt = torch.load(config.CKPT_PATH, map_location=DEVICE)
 
         if config.N_GPUS > 1 and config.MULTI_GPU:
-            disc.module.load_state_dict(ckpt["D"])
-            gen.module.load_state_dict(ckpt["G"])
+            D.module.load_state_dict(ckpt["D"])
+            G.module.load_state_dict(ckpt["G"])
         else:
-            disc.load_state_dict(ckpt["D"])
-            gen.load_state_dict(ckpt["G"])
-        disc_optim.load_state_dict(ckpt["D_optimizer"])
-        gen_optim.load_state_dict(ckpt["G_optimizer"])
+            D.load_state_dict(ckpt["D"])
+            G.load_state_dict(ckpt["G"])
+        D_optim.load_state_dict(ckpt["D_optimizer"])
+        G_optim.load_state_dict(ckpt["G_optimizer"])
         scaler.load_state_dict(ckpt["scaler"])
 
         if "average_swd" in ckpt:
@@ -187,8 +185,8 @@ if __name__ == "__main__":
     train_di = iter(train_dl)
     val_dl = get_dataloader(split="val", batch_size=batch_size, img_size=img_size)
 
-    disc_running_loss = 0
-    gen_running_loss = 0
+    D_running_loss = 0
+    G_running_loss = 0
     start_time = time()
     while True:
         step += 1
@@ -208,75 +206,76 @@ if __name__ == "__main__":
         with torch.autocast(
             device_type=DEVICE.type, dtype=torch.float16, enabled=True if config.AMP else False
         ):
-            real_pred = disc(real_image, img_size=img_size, alpha=alpha)
-            fake_image = gen(noise, img_size=img_size, alpha=alpha)
-            fake_pred = disc(fake_image.detach(), img_size=img_size, alpha=alpha)
+            real_pred = D(real_image, img_size=img_size, alpha=alpha)
+            fake_image = G(noise, img_size=img_size, alpha=alpha)
+            fake_pred = D(fake_image.detach(), img_size=img_size, alpha=alpha)
 
-            disc_loss1 = -torch.mean(real_pred) + torch.mean(fake_pred)
+            D_loss1 = -torch.mean(real_pred) + torch.mean(fake_pred)
+
             gp = get_gradient_penalty(
-                disc=disc,
+                D=D,
                 img_size=img_size,
                 alpha=alpha,
                 real_image=real_image,
                 fake_image=fake_image.detach(),
             )
+            D_loss2 = config.LAMBDA * gp
 
-            disc_loss2 = config.LAMBDA * gp
             # "We use the WGAN-GP loss."
             # "We introduce a fourth term into the discriminator loss with an extremely small weight
             # to keep the discriminator output from drifting too far away from zero. We set
             # $L' = L + \epsilon_{drift}\mathbb{E}_{x \in \mathbb{P}_{r}}[D(x)^{2}]$,
             # where $\epsilon_{drift} = 0.001$."
-            disc_loss3 = config.LOSS_EPS * torch.mean(real_pred ** 2)
-            disc_loss = disc_loss1 + disc_loss2 + disc_loss3
+            D_loss3 = config.LOSS_EPS * torch.mean(real_pred ** 2)
+            D_loss = D_loss1 + D_loss2 + D_loss3
 
-        disc_optim.zero_grad()
+        D_optim.zero_grad()
         if config.AMP:
-            scaler.scale(disc_loss).backward()
-            scaler.step(disc_optim)
+            scaler.scale(D_loss).backward()
+            scaler.step(D_optim)
         else:
-            disc_loss.backward()
-            disc_optim.step()
+            D_loss.backward()
+            D_optim.step()
 
         ### Optimize G.
-        freeze_model(disc)
+        freeze_model(D)
 
         with torch.autocast(
             device_type=DEVICE.type, dtype=torch.float16, enabled=True if config.AMP else False
         ):
-            fake_pred = disc(fake_image, img_size=img_size, alpha=alpha)
-            gen_loss = -torch.mean(fake_pred)
+            fake_pred = D(fake_image, img_size=img_size, alpha=alpha)
+            G_loss = -torch.mean(fake_pred)
 
-        gen_optim.zero_grad()
+        G_optim.zero_grad()
         if config.AMP:
-            scaler.scale(gen_loss).backward()
-            scaler.step(gen_optim)
+            scaler.scale(G_loss).backward()
+            scaler.step(G_optim)
         else:
-            gen_loss.backward()
-            gen_optim.step()
+            G_loss.backward()
+            G_optim.step()
 
-        unfreeze_model(disc)
+        unfreeze_model(D)
 
         if config.AMP:
             scaler.update()
 
-        disc_running_loss += disc_loss1.item()
-        gen_running_loss += gen_loss.item()
+        D_running_loss += D_loss1.item()
+        G_running_loss += G_loss.item()
 
         if (step % config.N_PRINT_STEPS == 0) or (step == n_steps):
-            disc_running_loss /= config.N_PRINT_STEPS
-            gen_running_loss /= config.N_PRINT_STEPS
+            D_running_loss /= config.N_PRINT_STEPS
+            G_running_loss /= config.N_PRINT_STEPS
 
             print(f"[ {img_size:,}×{img_size:,} ][ {step:,}/{n_steps:,} ][ {alpha:.3f} ]", end="")
             print(f"[ {get_elapsed_time(start_time)} ]", end="")
-            print(f"[ D loss: {disc_running_loss:.3f} ]", end="")
-            print(f"[ G loss: {gen_running_loss:.3f} ]", end="")
+            print(f"[ D loss: {D_running_loss:.3f} ]", end="")
+            print(f"[ G loss: {G_running_loss:.3f} ]", end="")
             print(f"[ GP: {gp.item():.3f} ]")
             start_time = time()
 
-            gen.eval()
+            G.eval()
             with torch.no_grad():
-                fake_image = gen(noise, img_size=img_size, alpha=alpha)
+                fake_image = G(noise, img_size=img_size, alpha=alpha)
                 fake_image = fake_image.detach().cpu()
                 grid = image_to_grid(
                     fake_image[: 9, ...], n_cols=2 if img_size == 1024 else 3, value_range=(-1, 1)
@@ -286,13 +285,13 @@ if __name__ == "__main__":
                 else:
                     save_path = IMG_DIR/f"{img_size}×{img_size}/{step}.jpg"
                 save_image(grid, path=save_path)
-            gen.train()
+            G.train()
 
-            disc_running_loss = 0
-            gen_running_loss = 0
+            D_running_loss = 0
+            G_running_loss = 0
 
         if (step % config.N_VAL_STEPS == 0) or (step == n_steps):
-            avg_swd = validate(gen=gen, val_dl=val_dl, device=DEVICE)
+            avg_swd = validate(G=G, val_dl=val_dl, device=DEVICE)
             if avg_swd < best_avg_swd:
                 if trans_phase:
                     cur_save_path = CKPT_DIR/\
@@ -304,10 +303,10 @@ if __name__ == "__main__":
                     step=step,
                     trans_phase=trans_phase,
                     avg_swd=avg_swd,
-                    disc=disc,
-                    gen=gen,
-                    disc_optim=disc_optim,
-                    gen_optim=gen_optim,
+                    D=D,
+                    G=G,
+                    D_optim=D_optim,
+                    G_optim=G_optim,
                     scaler=scaler,
                     save_path=cur_save_path,
                 )
